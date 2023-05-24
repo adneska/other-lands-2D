bfa96e6262acd020fb7a0cffb411d5513b075368
adneska <marvun72@gmail.com>
2023-05-24 22:34:07 +0300

    small improvements

--- Assets/scripts/Enemy/EnemySpawnHandler.cs
+++ Assets/scripts/Enemy/EnemySpawnHandler.cs
@@ -4,11 +4,11 @@ using UnityEngine;
 
 public class EnemySpawnHandler : MonoBehaviour
 {
-    [SerializeField] GameObject enemyPF;  // Префаб врага для спавна
-    private float spawnInterval = 3f;  // Промежуток времени между спаунами
-    private int maxEnemies = 5;  // Максимальное количество врагов на сцене    
+    [SerializeField] GameObject enemyPF;
+    private float spawnInterval = 3f;
+    private int maxEnemies = 5;
 
-    private static int currentEnemies = 0;  // Текущее количество врагов на сцене
+    private static int currentEnemies = 0;
 
     private void Start()
     {
@@ -19,27 +19,19 @@ public class EnemySpawnHandler : MonoBehaviour
     {
         while (true)
         {
-            // Проверяем, достигнуто ли максимальное количество врагов на сцене
             if (currentEnemies < maxEnemies)
             {
-                // Выбираем случайный дочерний объект
                 Transform randomChild = GetRandomChild();
                 if (randomChild != null)
                 {
-                    // Создаем нового врага на позиции выбранного дочернего объекта
                     GameObject newEnemy = Instantiate(enemyPF, randomChild.position, randomChild.rotation);
                     currentEnemies++;
                 }
-
-                // Увеличиваем сложность со временем, уменьшая промежуток между спаунами
                 spawnInterval *= 0.9f;
-
-                // Ожидаем заданный промежуток времени перед следующим спауном
                 yield return new WaitForSeconds(spawnInterval);
             }
             else
             {
-                // Если достигнуто максимальное количество врагов на сцене, ждем некоторое время
                 yield return new WaitForSeconds(1f);
             }
         }
@@ -52,10 +44,8 @@ public class EnemySpawnHandler : MonoBehaviour
 
     private Transform GetRandomChild()
     {
-        // Получаем все дочерние объекты
         Transform[] childObjects = GetComponentsInChildren<Transform>();
 
-        // Исключаем родительский объект из выборки
         List<Transform> validChildren = new List<Transform>();
         foreach (Transform child in childObjects)
         {
@@ -65,7 +55,6 @@ public class EnemySpawnHandler : MonoBehaviour
             }
         }
 
-        // Выбираем случайный дочерний объект
         if (validChildren.Count > 0)
         {
             return validChildren[Random.Range(0, validChildren.Count)];
